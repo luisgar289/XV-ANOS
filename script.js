4cf33ec19ef55a6734aec1038b596adc1bda9a57
luisgar289 <1720110205@utectulancingo.edu.mx>
2024-11-18 21:16:28 -0600

    Base Lista

---
+++ script.js
@@ -0,0 +1,7 @@
+const abrirInvitacion = document.getElementById('primerTexto');
+const invitacion = document.getElementById('invitacion')
+
+function verInvitacion() {
+    abrirInvitacion.style.display = 'none';
+    invitacion.style.display = 'flex';
+}
